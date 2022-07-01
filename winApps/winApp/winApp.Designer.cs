4ecb1f2c24b7159ac345559e087621f6f328be31
Tran Phuc <tranhongphuccdt@gmail.com>
2022-07-01 14:52:42 +0700

    new create

---
+++ winApps/winApp/winApp.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace main1
+{
+    partial class winApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cbPortName = new System.Windows.Forms.ComboBox();
+            this.txtBuff = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 19);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(78, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // cbPortName
+            // 
+            this.cbPortName.FormattingEnabled = true;
+            this.cbPortName.Location = new System.Drawing.Point(129, 21);
+            this.cbPortName.Name = "cbPortName";
+            this.cbPortName.Size = new System.Drawing.Size(124, 21);
+            this.cbPortName.TabIndex = 1;
+            // 
+            // txtBuff
+            // 
+            this.txtBuff.Location = new System.Drawing.Point(12, 94);
+            this.txtBuff.Multiline = true;
+            this.txtBuff.Name = "txtBuff";
+            this.txtBuff.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBuff.Size = new System.Drawing.Size(241, 79);
+            this.txtBuff.TabIndex = 2;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(130, 51);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(122, 20);
+            this.txtOut.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 187);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBuff);
+            this.Controls.Add(this.cbPortName);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cbPortName;
+        private System.Windows.Forms.TextBox txtBuff;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtOut;
+    }
+}
+
---
+++ winApps/winApp/winApp.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+
+
+namespace main1
+{
+    public partial class winApp : Form
+    {
+        public winApp()
+        {
+            InitializeComponent();
+        }
+
+        byte[] bytesReceived;
+
+        [StructLayout(LayoutKind.Explicit, Size = 16, Pack = 1)]
+        private struct MyStructType
+        {
+            [FieldOffset(0)]
+            public UInt16 Type;
+            [FieldOffset(2)]
+            public Byte DeviceNumber;
+            [FieldOffset(4)]
+            public float TableVersion;
+            [FieldOffset(8)]
+            public float SerialNumber;
+            [FieldOffset(12)]
+            public float Pi;
+            //[FieldOffset(16)]
+           // public Byte EndPayload;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] port = SerialPort.GetPortNames();
+            cbPortName.Items.AddRange(port);
+        }
+        
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cbPortName.Text;
+                serialPort1.BaudRate = 115200;
+                serialPort1.ReadBufferSize = 16;    //must be odd number
+                serialPort1.ReadTimeout = 500;
+                //serialPort1.ReadBufferSize = 16;
+                serialPort1.Open();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.ToString());
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //iBuff = serialPort1.ReadChar();
+            bytesReceived = new byte[16];
+            serialPort1.Read(bytesReceived, 0, 16);
+            this.Invoke(new EventHandler(showData));
+        }
+        private void showData(object sender, EventArgs e)
+        {
+            MyStructType receivedBuffer = new MyStructType();
+            receivedBuffer = StructTools.RawDeserialize<MyStructType>(bytesReceived, 0); // 0 is offset in byte[]
+            txtBuff.Text =  receivedBuffer.Type.ToString() + " " +
+                            receivedBuffer.DeviceNumber.ToString() + " " +
+                            receivedBuffer.TableVersion.ToString() + " " +
+                            receivedBuffer.SerialNumber.ToString() + " " +
+                            receivedBuffer.Pi.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyStructType sendBuffer = new MyStructType();
+            sendBuffer.Type = 1234;
+            sendBuffer.DeviceNumber = 56;
+            sendBuffer.TableVersion = 78901234;
+            sendBuffer.SerialNumber = 56789012;
+            sendBuffer.Pi = 3.14159265358979F;
+            byte[] bytes = StructTools.RawSerialize(sendBuffer);
+            //string sendString = Encoding.UTF8.GetString(bytes);       //not good method
+            //string sendStringAS = Encoding.ASCII.GetString(bytes);    //not good method
+            //string sendStringAS2 = Encoding.Unicode.GetString(bytes); //not good method
+            //serialPort1.Write(sendString);                            //not good method
+            serialPort1.Write(bytes, 0, bytes.Length);                  //OK
+        }
+    }
+}
+
+public static class StructTools
+{
+    /// <summary>
+    /// converts byte[] to struct
+    /// </summary>
+    public static T RawDeserialize<T>(byte[] rawData, int position)
+    {
+        int rawsize = Marshal.SizeOf(typeof(T));
+        if (rawsize > rawData.Length - position)
+            throw new ArgumentException("Not enough data to fill struct. Array length from position: " + (rawData.Length - position) + ", Struct length: " + rawsize);
+        IntPtr buffer = Marshal.AllocHGlobal(rawsize);
+        Marshal.Copy(rawData, position, buffer, rawsize);
+        T retobj = (T)Marshal.PtrToStructure(buffer, typeof(T));
+        Marshal.FreeHGlobal(buffer);
+        return retobj;
+    }
+
+    /// <summary>
+    /// converts a struct to byte[]
+    /// </summary>
+    public static byte[] RawSerialize(object anything)
+    {
+        int rawSize = Marshal.SizeOf(anything);
+        IntPtr buffer = Marshal.AllocHGlobal(rawSize);
+        Marshal.StructureToPtr(anything, buffer, false);
+        byte[] rawDatas = new byte[rawSize];
+        Marshal.Copy(buffer, rawDatas, 0, rawSize);
+        Marshal.FreeHGlobal(buffer);
+        return rawDatas;
+    }
+}
